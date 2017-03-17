6dd72ea254243ce4203e0c2c775c1f8f36a13abc
Yves Schelpe <yves.Schelpe@kdg.be>
2017-03-17 15:04:17 +0100

    Implemented POST

--- Module05_Associations/MyCodeCamp/Controllers/SpeakersController.cs
+++ Module05_Associations/MyCodeCamp/Controllers/SpeakersController.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
 using MyCodeCamp.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MyCodeCamp.Controllers
 {
@@ -35,8 +37,7 @@ namespace MyCodeCamp.Controllers
         }
 
         [HttpGet("{id}", Name = "GetSpeaker")]
-        public IActionResult Get(string moniker, int id)
-        {
+        public IActionResult Get(string moniker, int id) {
             try {
                 var speaker = _campRepository.GetSpeaker(id);
                 if(speaker == null) {
@@ -51,6 +52,29 @@ namespace MyCodeCamp.Controllers
             }
 
             return BadRequest($"Could nog get speaker");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(string moniker, [FromBody] SpeakerViewModel viewModel) {
+            try {
+                var camp = _campRepository.GetCampByMoniker(moniker);
+                if(camp == null) {
+                    return BadRequest("Could not get camp");
+                }
+
+                var speaker = _mapper.Map<Speaker>(viewModel);
+                speaker.Camp = camp;
+                _campRepository.Add(speaker);
+
+                if(await _campRepository.SaveAllAsync()) {
+                    var url = Url.Link("GetSpeaker", new { moniker = camp.Moniker, id = speaker.Id });
+                    return Created(url, _mapper.Map<SpeakerViewModel>(speaker));
+                }
+            } catch(Exception ex) {
+                _logger.LogCritical($"Exception thrown while saving a speaker: {ex}.");
+            }
+
+            return BadRequest("Could not add new speaker.");
         }
     }
 }
